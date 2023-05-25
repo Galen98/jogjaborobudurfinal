da52831ef240ecff23b28e175b1634b3789c166a
Macbook <macbook@Macbooks-MacBook-Air.local>
2023-05-26 01:37:07 +0700

    new newsdongs

--- resources/views/frontend/jajal.blade.php
+++ resources/views/frontend/jajal.blade.php
@@ -580,8 +580,8 @@ font-family: 'GT Eesti Text Trial', sans-serif;
     <h3 class="booking-assistant-configurator__header" data-v-dd428772  style="font-size:14px;font-weight: bolder;color:grey;margin-right: 10px;text-align:left;">{{$p->short}}</h3>
     <br>
     <h3 class="booking-assistant-configurator__header" data-v-dd428772 style="font-size:18px;font-weight: bolder;color:#182c4c;text-align:left;">Starting time:
-     <select name="waktu" style="width: 230px;border-radius: 10px 10px 10px 10px;" required="">
-        <option>Select a starting time</option>
+     <select name="waktu" style="font-size:16px;width: 180px;border-radius: 10px 10px 10px 10px;" required="">
+        <option >Select a starting time</option>
         @foreach($p->waktu as $w)<option value="{{ Carbon\Carbon::parse($w->time)->format('g:i A') }}">{{ Carbon\Carbon::parse($w->time)->format('g:i A') }}</option>@endforeach
       </select></h3>
       <h2 class="booking-assistant-configurator__header" data-v-dd428772 style="color: #182c4c;font-size:15px;text-align:left;" id="tanggal{{$p->id}}"></h2>
