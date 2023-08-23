526dc865813e1fa28a0242ce318f0aa44d0ef5fe
Macbook <macbook@Macbooks-MacBook-Air.local>
2023-08-24 00:47:35 +0700

    coba css city7

--- resources/views/frontend/city.blade.php
+++ resources/views/frontend/city.blade.php
@@ -111,7 +111,7 @@
   </div>
   </div>
 		@foreach($regions as $item)
-    <div class="gtco-container">
+    <!-- <div class="gtco-container">
       <div class="row">
         <div class="col-md-12 col-md-offset-0 text-left">
           <div class="row row-mt-15em">
@@ -123,7 +123,7 @@
           
         </div>
       </div>
-</div>
+</div> -->
       @endforeach
 	</header>
 	@endsection
