71c3786e2228cbb2bc54f99de5d80a57228c38a3
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-02-02 23:38:43 +0700

    2 feb 2024

--- resources/views/navadmin.blade.php
+++ resources/views/navadmin.blade.php
@@ -1,73 +1,82 @@
 @section('nav')
-        <li class="nav-item">
-          <a class="nav-link" href="/paketwisata">
-            <i class="mdi mdi-barley menu-icon"></i>
-            <span class="menu-title">Create Travel Packages</span>
+<li class="nav-item">
+          <a class="nav-link" href="/data-booking">
+            <i class="mdi mdi-wallet-travel menu-icon"></i>
+            <span class="menu-title">Bookings</span>
           </a>
         </li>
 
         <li class="nav-item">
           <a class="nav-link" href="/dateavailable">
             <i class="mdi mdi-calendar-multiple menu-icon"></i>
-            <span class="menu-title">Date Available</span>
+            <span class="menu-title">Availability</span>
           </a>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="/destination-category">
-            <i class="mdi mdi-apple-safari menu-icon"></i>
-            <span class="menu-title">Destination Category</span>
+          <a class="nav-link" href="/paketwisata">
+            <i class="mdi mdi-barley menu-icon"></i>
+            <span class="menu-title">Manage Tours</span>
           </a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="/province">
             <i class="mdi mdi-google-maps menu-icon"></i>
             <span class="menu-title">Province</span>
           </a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="/region">
             <i class="mdi mdi-map menu-icon"></i>
             <span class="menu-title">City</span>
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/tag-blog">
-            <i class="mdi mdi-tag-multiple menu-icon"></i>
-            <span class="menu-title">Tag Blog</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/data-booking">
-            <i class="mdi mdi-wallet-travel menu-icon"></i>
-            <span class="menu-title">Data Booking</span>
-          </a>
-        </li>
 
+        
+        <li class="nav-item">
+          <a class="nav-link" href="/destination-category">
+            <i class="mdi mdi-apple-safari menu-icon"></i>
+            <span class="menu-title">Point of Interest</span>
+          </a>
+        </li>
+        
         <li class="nav-item">
           <a class="nav-link" href="/blogadmin">
             <i class="mdi mdi-blogger menu-icon"></i>
             <span class="menu-title">Blog</span>
           </a>
         </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="/tag-blog">
+            <i class="mdi mdi-tag-multiple menu-icon"></i>
+            <span class="menu-title">Blog Tags</span>
+          </a>
+        </li>
+          
         <li class="nav-item">
           <a class="nav-link" href="/rating">
             <i class="mdi mdi-account-star menu-icon"></i>
-            <span class="menu-title">Rating</span>
+            <span class="menu-title">Reviews</span>
           </a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="/bahasa">
             <i class="mdi mdi-google-translate menu-icon"></i>
-            <span class="menu-title">Bahasa</span>
+            <span class="menu-title">Language</span>
           </a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="/background/change">
             <i class="mdi mdi-collage menu-icon"></i>
-            <span class="menu-title">Change Background</span>
+            <span class="menu-title">Background</span>
           </a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="/season">
             <i class="mdi mdi mdi-google-maps
@@ -75,10 +84,11 @@
             <span class="menu-title">Theme</span>
           </a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="/currency/currency">
             <i class=" mdi mdi-currency-usd menu-icon"></i>
-            <span class="menu-title">Custom Currency</span>
+            <span class="menu-title">Currency</span>
           </a>
         </li>
         <li class="nav-item">
